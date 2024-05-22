45ec2fad1a697ec62936221d5eb7b1ea8a6c151f
Jordane <jordane.jouffroy@outlook.com>
2024-05-22 21:32:36 +0200

    bump 2.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'common'
 name 'vote'
 description 'Vote plugin for Top-games.net/Top-serveurs.net platforms'
 author 'Top-Games/Top-Serveurs'
-version '1.1.3'
+version '2.0.0'
 url 'https://github.com/Top-Serveurs/cfx-vote-plugin'
 
 
